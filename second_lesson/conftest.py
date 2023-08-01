186d30089ffe16488f5541e2f836f8a985af72d5
AlterAyrol <lazarev.reach@gmail.com>
2023-08-01 20:21:57 +0300

    second lesson done

---
+++ second_lesson/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selene.support.shared import browser
+
+
+@pytest.fixture(autouse=True)
+def web_browser():
+    browser.open('https://google.com')
+    browser.config.window_width = 1400
+    browser.config.window_height = 1600
+    yield
+    browser.quit()
