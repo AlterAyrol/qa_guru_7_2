f0e609d600be5ccb0097a829f90a42aa0ab66197
AlterAyrol <lazarev.reach@gmail.com>
2023-08-01 22:36:58 +0300

    relocate browser.open('https://google.com') from conftest in test_second_lesson

--- second_lesson/test_second_lesson.py
+++ second_lesson/test_second_lesson.py
@@ -4,12 +4,14 @@ from selene.support.shared import browser
 
 
 def test_1(web_browser):
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
     browser.quit()
 
 
 def test_2(web_browser):
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('qweqweq1231231weqweqwe').press_enter()
     browser.element('.card-section [aria-level="3"]').should(have.text('По запросу qweqweq1231231weqweqwe ничего не найдено. '))
     browser.quit()
