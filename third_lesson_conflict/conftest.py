c06f984339cd2e7c55d605ead17a79b937d7eb04
AlterAyrol <lazarev.reach@gmail.com>
2023-08-02 20:27:17 +0300

    init third lesson

---
+++ third_lesson_conflict/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from selene.support.shared import browser
+
+
+@pytest.fixture(autouse=True)
+def web_browser():
+    browser.config.window_width = 1400
+    browser.config.window_height = 1600
+
