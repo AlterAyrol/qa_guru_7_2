d4e41840ade4a280acb0c6ca18466b50e24d212d
AlterAyrol <lazarev.reach@gmail.com>
2023-08-10 23:27:03 +0300

    refactor lesson_05 refactor name< last name, address, subjects, hobby, send picture

--- lesson_05/test_lesson_05.py
+++ lesson_05/test_lesson_05.py
@@ -1,5 +1,6 @@
 from selene import browser
 from selene import be, have, command
+import os
 
 
 #locators
@@ -7,22 +8,22 @@ first_name_locator = '//input[@id="firstName"]'
 last_name_locator = '//input[@id="lastName"]'
 email_locator = '//input[@id="userEmail"]'
 
-gender_male_locator = '//input[@id="gender-radio-1"]'
-gender_female_locator = '//input[@id="gender-radio-2"]'
-gender_other_locator = '//input[@id="gender-radio-3"]'
+gender_male_locator = '//label[@for="gender-radio-1"]'
+gender_female_locator = '//label[@for="gender-radio-2"]'
+gender_other_locator = '//label[@for="gender-radio-3"]'
 
 mobile_locator = '//input[@id="userNumber"]'
 
 date_of_birth_locator = '//input[@id="dateOfBirthInput"]'
 month_locator = '//select[@class="react-datepicker__month-select"]'
 year_locator = '//select[@class="react-datepicker__year-select"]'
-day_container_locator = '//select[@class="react-datepicker__month-container"]'
+day_container_locator = '//div[@class="react-datepicker__day react-datepicker__day--014"]'
 
-subjects_locator = '//input[@id="subjectsContainer"]'
+subjects_locator = '//input[@id="subjectsInput"]]'
 
-hobbies_sports_locator = '//input[@id="hobbies-checkbox-1"]'
-hobbies_reading_locator = '//input[@id="hobbies-checkbox-2"]'
-hobbies_music_locator = '//input[@id="hobbies-checkbox-3"]'
+hobbies_sports_locator = '//label[@for="hobbies-checkbox-1""]'
+hobbies_reading_locator = '//label[@for="hobbies-checkbox-2"]'
+hobbies_music_locator = '//label[@for="hobbies-checkbox-3"]'
 
 picture_locator = '//input[@id="uploadPicture"]'
 address_locator = '//input[@id="currentAddress"]'
@@ -42,7 +43,7 @@ def test_5():
     mobile = '0123456789'
     birth_month = 'jan'
     birth_year = '1988'
-    birth_date = '14'
+    subject = 'some subject'
     address = 'city One and street Two'
     state = 'NCR'
     city = 'Noida'
@@ -51,30 +52,33 @@ def test_5():
     #открытие нужной страницы
     browser.open('/automation-practice-form')
 
-    #Ввод имени и фамилии
-    browser.element(first_name_locator).should(be.blank).type(first_name)
-    browser.element(last_name_locator).should(be.blank).type(last_name)
+    # #Ввод имени и фамилии
+    # browser.element(first_name_locator).should(be.blank).type(first_name)
+    # browser.element(last_name_locator).should(be.blank).type(last_name)
+    #
+    # #Ввод емейла
+    # browser.element(email_locator).should(be.blank).type(email)
+    #
+    # #Выбор пола
+    # browser.element(gender_male_locator).click()
+    #
+    # #Ввод номера мобильного телефона
+    # browser.element(mobile_locator).should(be.blank).type(mobile)
+    #
+    # #Открытие календаря и выбор даты рождения
+    # browser.element(date_of_birth_locator).click()
+    # browser.element(month_locator).type(birth_month).press_enter()
+    # browser.element(year_locator).type(birth_year).press_enter()
+    # browser.element(day_container_locator).click()
 
-    #Ввод емейла
-    browser.element(email_locator).should(be.blank).type(email)
-
-    #Выбор пола
-    browser.element(gender_male_locator).click()
-
-    #Ввод номера мобильного телефона
-    browser.element(mobile_locator).should(be.blank).type(mobile)
-
-    #Открытие календаря и выбор даты рождения
-    browser.element(date_of_birth_locator).click()
-    browser.element(month_locator).type(birth_month).press_enter()
-    browser.element(year_locator).type(birth_year).press_enter()
-    browser.all(day_container_locator).element_by(have.text(birth_date)).click()
+    # # Ввод тематики
+    # browser.element(subjects_locator).click().type(subject)
 
     #Выбор хобби
-    browser.element(hobbies_reading_locator).should(be.disabled).click()
-
-    #Отправка картинки
-    browser.element(picture_locator).send_keys('../date_for_sending/for_send.bmp')
+    # browser.element(hobbies_reading_locator).click()
+    #
+    # #Отправка картинки
+    # browser.element(picture_locator).send_keys(os.path.abspath('date_for_send/for_send.bmp'))
 
     #Ввод адреса
     browser.element(address_locator).should(be.blank).type(address)
