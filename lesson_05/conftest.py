af5c0e8dbecbcb15476133d204b0b9b0e94b3681
su.lazarev <su.lazarev@tensor.ru>
2023-08-10 10:43:58 +0300

    добавил базовую url

--- lesson_05/conftest.py
+++ lesson_05/conftest.py
@@ -6,10 +6,11 @@ from selenium import webdriver
 @pytest.fixture(autouse=True)
 def web_browser():
     driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless')
+    # driver_options.add_argument('--headless')
     browser.config.driver_options = driver_options
     browser.config.window_width = 1400
     browser.config.window_height = 1600
+    browser.config.base_url = 'https://demoqa.com'
 
     yield
 
