61d4fc032d1f819e5f7e53a9f0152327e030f70d
AlterAyrol <lazarev.reach@gmail.com>
2023-08-11 22:49:35 +0300

    headless added in conftest

--- lesson_05/conftest.py
+++ lesson_05/conftest.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 @pytest.fixture(autouse=True)
 def web_browser():
     driver_options = webdriver.ChromeOptions()
-    # driver_options.add_argument('--headless')
+    driver_options.add_argument('--headless')
     browser.config.driver_options = driver_options
     browser.config.window_width = 1400
     browser.config.window_height = 1600
