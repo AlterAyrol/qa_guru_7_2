f43f281217d4ec1e8f2cd5d96ed6800e2d19aeed
AlterAyrol <lazarev.reach@gmail.com>
2023-08-11 22:48:05 +0300

    refactor lesson_05 refactor adress, form submit and check

--- lesson_05/test_lesson_05.py
+++ lesson_05/test_lesson_05.py
@@ -19,17 +19,17 @@ month_locator = '//select[@class="react-datepicker__month-select"]'
 year_locator = '//select[@class="react-datepicker__year-select"]'
 day_container_locator = '//div[@class="react-datepicker__day react-datepicker__day--014"]'
 
-subjects_locator = '//input[@id="subjectsInput"]]'
+subjects_locator = '//input[@id="subjectsInput"]'
 
 hobbies_sports_locator = '//label[@for="hobbies-checkbox-1""]'
 hobbies_reading_locator = '//label[@for="hobbies-checkbox-2"]'
 hobbies_music_locator = '//label[@for="hobbies-checkbox-3"]'
 
 picture_locator = '//input[@id="uploadPicture"]'
-address_locator = '//input[@id="currentAddress"]'
+address_locator = '//textarea[@id="currentAddress"]'
 
-select_state_locator = '//input[@id="state"]'
-select_city_locator = '//input[@id="city"]'
+select_state_locator = '//input[@id="react-select-3-input"]'
+select_city_locator = '//input[@id="react-select-4-input"]'
 
 submit_button_locator = '//button[@id="submit"]'
 submitting_form_locator = '//div[@id="example-modal-sizes-title-lg"]'
@@ -43,7 +43,7 @@ def test_5():
     mobile = '0123456789'
     birth_month = 'jan'
     birth_year = '1988'
-    subject = 'some subject'
+    subject = 'eng'
     address = 'city One and street Two'
     state = 'NCR'
     city = 'Noida'
@@ -52,33 +52,33 @@ def test_5():
     #открытие нужной страницы
     browser.open('/automation-practice-form')
 
-    # #Ввод имени и фамилии
-    # browser.element(first_name_locator).should(be.blank).type(first_name)
-    # browser.element(last_name_locator).should(be.blank).type(last_name)
-    #
-    # #Ввод емейла
-    # browser.element(email_locator).should(be.blank).type(email)
-    #
-    # #Выбор пола
-    # browser.element(gender_male_locator).click()
-    #
-    # #Ввод номера мобильного телефона
-    # browser.element(mobile_locator).should(be.blank).type(mobile)
-    #
-    # #Открытие календаря и выбор даты рождения
-    # browser.element(date_of_birth_locator).click()
-    # browser.element(month_locator).type(birth_month).press_enter()
-    # browser.element(year_locator).type(birth_year).press_enter()
-    # browser.element(day_container_locator).click()
+    #Ввод имени и фамилии
+    browser.element(first_name_locator).should(be.blank).type(first_name)
+    browser.element(last_name_locator).should(be.blank).type(last_name)
 
-    # # Ввод тематики
-    # browser.element(subjects_locator).click().type(subject)
+    #Ввод емейла
+    browser.element(email_locator).should(be.blank).type(email)
 
-    #Выбор хобби
-    # browser.element(hobbies_reading_locator).click()
-    #
-    # #Отправка картинки
-    # browser.element(picture_locator).send_keys(os.path.abspath('date_for_send/for_send.bmp'))
+    #Выбор пола
+    browser.element(gender_male_locator).click()
+
+    #Ввод номера мобильного телефона
+    browser.element(mobile_locator).should(be.blank).type(mobile)
+
+    #Открытие календаря и выбор даты рождения
+    browser.element(date_of_birth_locator).click()
+    browser.element(month_locator).type(birth_month).press_enter()
+    browser.element(year_locator).type(birth_year).press_enter()
+    browser.element(day_container_locator).click()
+
+    # Ввод тематики
+    browser.element(subjects_locator).click().type(subject).press_enter()
+
+    # Выбор хобби
+    browser.element(hobbies_reading_locator).click()
+
+    #Отправка картинки
+    browser.element(picture_locator).send_keys(os.path.abspath('date_for_send/for_send.bmp'))
 
     #Ввод адреса
     browser.element(address_locator).should(be.blank).type(address)
