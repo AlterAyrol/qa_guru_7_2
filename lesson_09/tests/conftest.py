f7d53a68dfcff50f25060984623677d90d350e64
AlterAyrol <lazarev.reach@gmail.com>
2023-08-19 20:38:19 +0300

    add conftest in lesson_09

---
+++ lesson_09/tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from selene import browser
+from selenium import webdriver
+
+
+@pytest.fixture(autouse=True)
+def web_browser():
+    driver_options = webdriver.ChromeOptions()
+    # driver_options.add_argument('--headless')
+    browser.config.driver_options = driver_options
+    browser.config.window_width = 1400
+    browser.config.window_height = 1600
+
+    yield
+
+    browser.quit()
