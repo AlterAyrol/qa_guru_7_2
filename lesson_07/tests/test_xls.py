60b6f6c3200f2e160c086709f5cea480b7631309
AlterAyrol <lazarev.reach@gmail.com>
2023-08-20 20:27:37 +0300

    init and done

--- lesson_07/tests/test_xls.py
+++ lesson_07/tests/test_xls.py
@@ -1,13 +1,30 @@
 import xlrd
+import os
+from conftest import RESOURCES_DIR
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
 
-book = xlrd.open_workbook('resources/file_example_XLS_10.xls')
 
-print(f'Количество листов {book.nsheets}')
-print(f'Имена листов {book.sheet_names()}')
-sheet = book.sheet_by_index(0)
-print(f'Количество колонок  {sheet.ncols}')
-print(f'Количество строк    {sheet.nrows}')
-print(f'Пересечение строки и столбца {sheet.cell_value(rowx=3, colx=2)}')
-for rx in range(sheet.nrows):
-    print(sheet.row(rx))
+def test_xls():
+    book = xlrd.open_workbook(os.path.join(RESOURCES_DIR, 'file_example_XLS_10.xls'))
+
+    print(f'Количество листов {book.nsheets}')
+    print(f'Имена листов {book.sheet_names()}')
+    sheet = book.sheet_by_index(0)
+    print(f'Количество колонок  {sheet.ncols}')
+    print(f'Количество строк    {sheet.nrows}')
+    print(f'Пересечение строки и столбца {sheet.cell_value(rowx=3, colx=2)}')
+    assert book.nsheets == 1
+    assert book.sheet_names() == ['Sheet1']
+    assert sheet.ncols == 8
+    assert sheet.nrows == 10
+    assert sheet.cell_value(rowx=3, colx=2) == 'Gent'
+    rows = []
+    for rx in range(sheet.nrows):
+        rows.append(sheet.row(rx))
+        print(sheet.row(rx))
+    assert book.nsheets == 1
+    assert book.sheet_names() == ['Sheet1']
+    assert sheet.ncols == 8
+    assert sheet.nrows == 10
+    assert sheet.cell_value(rowx=3, colx=2) == 'Gent'
+    assert len(rows) == 10
